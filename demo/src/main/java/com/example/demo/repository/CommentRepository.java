7d53c11aa7c38cbf21be1258b4174cfe5bd84643
kimhyunso <gustn9515@gmail.com>
2024-01-03 01:01:28 +0900

    20240103 01:01

--- demo/src/main/java/com/example/demo/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
